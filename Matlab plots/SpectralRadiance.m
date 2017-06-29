db3758ebe781751e8e0e495aa88ff6ef3675bc74
AndreBestbier <abestbier@gmail.com>
2017-06-29 15:57:46 +0200

    Lit study up to BCG
    Everything to just before HR through BCG is copied to Letex. Figures and refs working. Compiling well. Stil some proofreading and editing needed. Byt just whant a backup, for the PPG section is large

--- Matlab plots/SpectralRadiance.m
+++ Matlab plots/SpectralRadiance.m
@@ -5,21 +5,39 @@ h = 6.626070040e-34;    %Planck's constant
 c = 299792458;          %Speed of light
 kb = 1.38064852e-23;    %Boltzmann constant
 lambda = (1:0.01:100).*(10^-6);  %Wave length
-T1 = 310.15;                %Temperature
-T2 = 273.15;                %Temperature
-T3 = 373.15;                %Temperature
+T1 = 308.15;                %Temperature
+T2 = 310.15;                %Temperature
+T3 = 312.15;                %Temperature
 
 for i=1:length(lambda)
 B1(i) = ((2*h*c^2)/(lambda(i)^5))*(1/(exp((h*c)/(lambda(i)*kb*T1))-1));
 end
 
+for i=1:length(lambda)
+B2(i) = ((2*h*c^2)/(lambda(i)^5))*(1/(exp((h*c)/(lambda(i)*kb*T2))-1));
+end
+
+for i=1:length(lambda)
+B3(i) = ((2*h*c^2)/(lambda(i)^5))*(1/(exp((h*c)/(lambda(i)*kb*T3))-1));
+end
 
 
-plot(lambda*1000000, B1/1000000, 'k'); grid;
+
+
+
+
+
+
+
+
+plot(lambda*1000000, B1/1000000); grid; hold on;
+plot(lambda*1000000, B2/1000000);
+plot(lambda*1000000, B3/1000000);
 title('Planck''s Law at T = 37 deg C');
 xlabel('Wavelength (\mum)');
 ylabel('Spectral Radiance (kW·sr^{-1}·m^{-2}·nm^{-1})');
-axis([0 100 0 14]);
+legend('35 deg C','37 deg C','39 deg C');
+axis([0 100 0 14]); hold off;
 
 
 
